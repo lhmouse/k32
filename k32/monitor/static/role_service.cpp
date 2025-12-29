8e90f9142153689762fd15ea7788f70420467b92
LIU Hao <lh_mouse@126.com>
2025-12-29 17:40:39 +0800

    *: Fold do..while statements

--- k32/monitor/static/role_service.cpp
+++ k32/monitor/static/role_service.cpp
@@ -507,8 +507,7 @@ do_star_role_unload(const shptr<Implementation>& impl, ::poseidon::Abstract_Fibe
       task2 = new_sh<::poseidon::Redis_Query_Future>(::poseidon::redis_connector, redis_cmd);
       ::poseidon::task_scheduler.launch(task2);
       fiber.yield(task2);
-    }
-    while(!task2->result().is_nil());
+    } while(!task2->result().is_nil());
     impl->role_records.erase(roid);
 
     POSEIDON_LOG_INFO(("Unloaded role `$1` (`$2`)"), roinfo.roid, roinfo.nickname);
